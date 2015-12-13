734a60a1c40db85f15145df5cbb99a9bf2f7677a
Fred Sauer <fredsa@google.com>
2015-12-12 20:05:54 -0800

    tweak

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -8,8 +8,8 @@ public class PlayerController : NetworkBehaviour
 	public int gridSize = 10;
 	public GameObject gridPrefab;
 
-	public Color color1 = new Color (1f, 0f, 0f, .4f);
-	public Color color2 = new Color (0f, 1f, 0f, .4f);
+	public Color color1 = new Color (1f, 0f, 0f, .1f);
+	public Color color2 = new Color (0f, 1f, 0f, .1f);
 
 	void Start ()
 	{	
@@ -18,8 +18,8 @@ public class PlayerController : NetworkBehaviour
 		GameObject grid2 = Instantiate (gridPrefab, new Vector3 (0f, 0.2f, gridSize), rot) as GameObject;
 		grid1.name = "Grid (Mine)";
 		grid2.name = "Grid (Theirs)";
-		grid1.GetComponent<GridHolderController> ().RecreateGrid (gridSize, Color.red);
-		grid2.GetComponent<GridHolderController> ().RecreateGrid (gridSize, Color.green);
+		grid1.GetComponent<GridHolderController> ().RecreateGrid (gridSize, color1);
+		grid2.GetComponent<GridHolderController> ().RecreateGrid (gridSize, color2);
 	}
 	
 }
