23c16dfd1e3b7dc847b15b16eb8661ac737cd07b
Fred Sauer <fredsa@google.com>
2015-12-23 11:46:54 -0800

    fizzle out volume

--- Assets/_Scripts/RocketController.cs
+++ Assets/_Scripts/RocketController.cs
@@ -1,22 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof(AudioSource))]
 public class RocketController : MonoBehaviour
 {
 	ParticleSystem flameParticleSystem;
+	AudioSource source;
 	//	Transform targetTransform;
 	float velocity = 1.5f;
+	float[] fizzleOutTimes;
 
 	void Awake ()
 	{
 		GameController.instance.SetIsFiring (true);
 		flameParticleSystem = GetComponentInChildren<ParticleSystem> ();
+		source = GetComponent<AudioSource> ();
 	}
 
 	void Update ()
 	{
 		if (Input.GetButtonUp ("Fire1")) {
 			Time.timeScale = 10f;
+		}
+		if (fizzleOutTimes != null) {
+			source.volume = (fizzleOutTimes [1] - Time.time) / (fizzleOutTimes [1] - fizzleOutTimes [0]);
 		}
 	}
 
@@ -57,15 +64,16 @@ public class RocketController : MonoBehaviour
 			Debug.LogError ("Unexpected collision with " + other);
 		}
 
-		FizzleOut ();
+		FizzleOut (flameParticleSystem.duration);
 	}
 
-	void FizzleOut ()
+	void FizzleOut (float duration)
 	{
+		fizzleOutTimes = new float[]{ Time.time, Time.time + duration };
 		flameParticleSystem.Stop ();
 		//disable mesh
 		transform.GetChild (0).gameObject.SetActive (false);
-		Destroy (gameObject, flameParticleSystem.duration);
+		Destroy (gameObject, duration);
 	}
 
 }
