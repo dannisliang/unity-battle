03bfb9ca3a43f47cd16aae9e7341d00ff4fdf21e
Fred Sauer <fredsa@google.com>
2015-12-29 15:05:18 -0800

    'G" is for grid

--- Assets/_Scripts/Realtime/RealtimeBattleship.cs
+++ Assets/_Scripts/Realtime/RealtimeBattleship.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Assertions;
 using System.Collections;
 using System.IO;
 using System.Runtime.Serialization;
@@ -10,6 +11,7 @@ public class RealtimeBattleship : MonoBehaviour
 	{
 		BinaryFormatter formatter = new BinaryFormatter ();
 		using (MemoryStream stream = new MemoryStream ()) {
+			stream.WriteByte ((byte)'G');
 			formatter.Serialize (stream, grid);
 			byte[] bytes = stream.ToArray ();
 			return bytes;
@@ -20,6 +22,7 @@ public class RealtimeBattleship : MonoBehaviour
 	{
 		BinaryFormatter formatter = new BinaryFormatter ();
 		using (MemoryStream stream = new MemoryStream (bytes)) {
+			Assert.IsTrue (stream.ReadByte () == 'G');
 			Grid grid = formatter.Deserialize (stream) as Grid;
 			return grid;
 		}
