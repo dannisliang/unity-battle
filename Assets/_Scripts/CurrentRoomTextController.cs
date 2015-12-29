b19524c13b6feb57285bb31d2d9420920359690f
Fred Sauer <fredsa@google.com>
2015-12-28 18:35:21 -0800

    show # connected participants when in a room

---
+++ Assets/_Scripts/CurrentRoomTextController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using GooglePlayGames;
+
+public class CurrentRoomTextController : MonoBehaviour
+{
+
+	Text text;
+
+	void Start ()
+	{
+		text = GetComponent<Text> ();
+	}
+
+	void Update ()
+	{
+		text.text = PlayGamesPlatform.Instance.IsAuthenticated () && PlayGamesPlatform.Instance.RealTime.IsRoomConnected () ?
+			"Room with " + PlayGamesPlatform.Instance.RealTime.GetConnectedParticipants ().Count + " participants" : "(not in a room)";
+	}
+}
