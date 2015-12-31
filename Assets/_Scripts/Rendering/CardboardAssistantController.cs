a2de14e75887b1d253dde29971006d7768cef4c3
Fred Sauer <fredsa@google.com>
2015-12-30 17:16:41 -0800

    fix cardboard assistant

--- Assets/_Scripts/Rendering/CardboardAssistantController.cs
+++ Assets/_Scripts/Rendering/CardboardAssistantController.cs
@@ -4,12 +4,12 @@ using System.Collections;
 
 public class CardboardAssistantController : MonoBehaviour
 {
-	public Toggle cardboardToggle;
+	//	public Toggle cardboardToggle;
 
 	void Awake ()
 	{
 		Cardboard.SDK.TapIsTrigger = false;
-		cardboardToggle.isOn = Cardboard.SDK.VRModeEnabled;
+//		cardboardToggle.isOn = Cardboard.SDK.VRModeEnabled;
 #if UNITY_EDITOR
 		Cardboard.SDK.StereoScreenScale = 2f;
 #endif
