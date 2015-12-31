4f5d373ace6423751be2dba609b22b6640fa6efb
Fred Sauer <fredsa@google.com>
2015-12-31 10:44:10 -0800

    drag threshold on main menu

---
+++ Assets/_Scripts/0_MainMenu/VrModeButtonController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class VrModeButtonController : MonoBehaviour
+{
+
+	Button toggle;
+
+	void Awake ()
+	{
+		toggle = GetComponent<Button> ();
+		toggle.onClick.AddListener (delegate {
+			Prefs.VrMode = true;
+			CardboardAssistantController.instance.ApplyCardboardSettings ();
+		});
+	}
+
+}
