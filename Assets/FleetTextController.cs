a3099fe279010de6c01cb221b2da87aafb210710
Fred Sauer <fredsa@google.com>
2016-01-03 11:27:58 -0800

    http://www.navy.mil/navydata/our_ships.asp world space canvas side panels

---
+++ Assets/FleetTextController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent (typeof(Text))]
+public class FleetTextController : MonoBehaviour
+{
+
+	void OnValidate ()
+	{
+		string t = "";
+		for (int i = 0; i < Grid.fleet.Length; i++) {
+			Grid.BoatConfiguration config = Grid.fleet [i];
+			t += config.designation + "\n- " + config.size + " units\n\n";
+		}
+		GetComponent<Text> ().text = t;
+	}
+	
+}
--- Assets/_Scripts/1_BattleshipGame/Grid.cs
+++ Assets/_Scripts/1_BattleshipGame/Grid.cs
@@ -4,16 +4,35 @@ using System.Collections;
 [System.Serializable]
 public class Grid
 {
+	public struct BoatConfiguration
+	{
+		public int size;
+		public string designation;
 
-	static int[] boatSizes = { 5, 4, 3, 2, 2, 1 };
+		public BoatConfiguration (int size, string designation)
+		{
+			this.size = size;
+			this.designation = designation;
+		}
+	}
+
+	// http://www.navy.mil/navydata/our_ships.asp
+	public static BoatConfiguration[] fleet = {
+		new BoatConfiguration (5, "Aircraft Carrier"),
+		new BoatConfiguration (4, "Amphibious Assault"),
+		new BoatConfiguration (3, "Cruiser"),
+		new BoatConfiguration (2, "Littoral Combat"),
+		new BoatConfiguration (2, "Destroyer"),
+		new BoatConfiguration (1, "Submarine"),
+	};
 
 	public Boat[] boats;
 
 	public void RandomizeBoats ()
 	{
-		boats = new Boat[boatSizes.Length];
-		for (int i = 0; i < boatSizes.Length; i++) {
-			int size = boatSizes [i];
+		boats = new Boat[fleet.Length];
+		for (int i = 0; i < fleet.Length; i++) {
+			int size = fleet [i].size;
 
 			bool conflict = true;
 			while (conflict) {
