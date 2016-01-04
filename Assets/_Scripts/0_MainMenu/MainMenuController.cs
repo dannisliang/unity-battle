218c69c6f4c13b98690ff06dec636d54eae31e1b
Fred Sauer <fredsa@google.com>
2016-01-04 15:50:26 -0800

    main menu layout change and smart game level loading

---
+++ Assets/_Scripts/0_MainMenu/MainMenuController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour
+{
+
+	void Awake ()
+	{
+		if (Time.frameCount <= 1 && GameController.instance == null) {
+			// load main game level
+			SceneManager.LoadScene (Utils.SCENE_BATTLESHIP_GAME);
+			// prevent NPEs due to non-existent GameController
+			gameObject.SetActive (false);
+		}	
+	}
+}
