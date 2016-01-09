e191fbc4a3ef8d26def0d30129d4e9a965aebb59
Fred Sauer <fredsa@google.com>
2016-01-09 10:10:33 -0800

    Workaround StereoController.keepStereoUpdated failing to update render texture size when game view is resized in editor

---
+++ Assets/_Scripts/Rendering/CardboardRenderTextureWorkaround.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardboardRenderTextureWorkaround : MonoBehaviour
+{
+
+	int width, height;
+
+	void Awake ()
+	{
+		if (!Application.isEditor) {
+			Destroy (gameObject);
+		}
+	}
+
+	void Update ()
+	{
+		// Workaround StereoController.keepStereoUpdated failing to
+		// update render texture size when game view is resized in editor
+		if (Screen.width != width || Screen.height != height) {
+			ForceNewRenderTexture (CardboardProfile.DeviceTypes.GoggleTechC1Glass);
+			ForceNewRenderTexture (CardboardProfile.DeviceTypes.CardboardMay2015);
+			width = Screen.width;
+			height = Screen.height;
+		}
+	}
+
+	void ForceNewRenderTexture (CardboardProfile.DeviceTypes deviceType)
+	{
+		foreach (Camera camera in Camera.main.GetComponent<StereoController> ().GetComponentsInChildren<Camera>()) {
+			camera.targetTexture = null;
+		}
+		if (Cardboard.SDK.StereoScreen != null) {
+			Cardboard.SDK.StereoScreen.Release ();
+		}
+		Cardboard.SDK.StereoScreen = null;
+		Cardboard.SDK.DeviceType = deviceType;
+	}
+
+}
