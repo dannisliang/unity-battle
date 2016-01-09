d434019ca00d88d7191b0598e224a7dc81f0b5e1
Fred Sauer <fredsa@google.com>
2016-01-09 01:00:27 -0800

    stereo screen scale 0.7

--- Assets/_Scripts/Rendering/CardboardAssistantController.cs
+++ Assets/_Scripts/Rendering/CardboardAssistantController.cs
@@ -18,7 +18,10 @@ public class CardboardAssistantController : MonoBehaviour
 		//		Cardboard.SDK.ElectronicDisplayStabilization = false;
 		Cardboard.SDK.AutoDriftCorrection = false;
 		Cardboard.SDK.EnableSettingsButton = true;
+		Cardboard.SDK.StereoScreenScale = Application.isEditor ? 1f : .7f;
 		VrModeChanged (Prefs.VrMode);
+		// just in case VrMode already set to default
+		Camera.main.GetComponent<StereoController> ().UpdateStereoValues ();
 	}
 
 	void OnEnable ()
