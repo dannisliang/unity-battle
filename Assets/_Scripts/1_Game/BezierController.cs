8fb8a25ad0847104f353b5c83d014b9318ef0f02
Fred Sauer <fredsa@google.com>
2016-01-16 10:28:37 -0800

    bezier editor

---
+++ Assets/_Scripts/1_Game/BezierController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BezierController : MonoBehaviour
+{
+	public class Points
+	{
+		public Transform t0;
+		public Transform t1;
+		public Transform t2;
+		public Transform t3;
+	}
+
+	[HideInInspector]
+	public Points points;
+
+	void OnValidate ()
+	{
+		MayInitializePoints ();
+	}
+
+	void Awake ()
+	{
+		MayInitializePoints ();
+	}
+
+	void MayInitializePoints ()
+	{
+		if (points != null) {
+			return;
+		}
+		points = new Points ();
+		points.t0 = transform.GetChild (0);
+		points.t1 = transform.GetChild (1);
+		points.t2 = transform.GetChild (2);
+		points.t3 = transform.GetChild (3);
+	}
+
+	public Vector3 GetPoint (float t)
+	{
+		return GetPoint (points.t0.position, points.t1.position, points.t2.position, points.t3.position, t);
+	}
+
+	public Vector3 GetVelocity (float t)
+	{
+		return GetFirstDerivative (points.t0.position, points.t1.position, points.t2.position, points.t3.position, t);
+	}
+
+	public static Vector3 GetPoint (Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+	{
+		t = Mathf.Clamp01 (t);
+		float oneMinusT = 1f - t;
+		return
+			oneMinusT * oneMinusT * oneMinusT * p0 +
+		3f * oneMinusT * oneMinusT * t * p1 +
+		3f * oneMinusT * t * t * p2 +
+		t * t * t * p3;
+	}
+
+	public static Vector3 GetFirstDerivative (Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+	{
+		t = Mathf.Clamp01 (t);
+		float oneMinusT = 1f - t;
+		return
+			3f * oneMinusT * oneMinusT * (p1 - p0) +
+		6f * oneMinusT * t * (p2 - p1) +
+		3f * t * t * (p3 - p2);
+	}
+
+}
---
+++ Assets/_Scripts/1_Game/Editor/BezierEditor.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using UnityEditor.SceneManagement;
+
+[CustomEditor (typeof(BezierController))]
+[CanEditMultipleObjects]
+public class BezierEditor : Editor
+{
+	const int STEPS = 10;
+
+	private void OnSceneGUI ()
+	{
+		BezierController bezier = target as BezierController;
+
+		Handles.color = Color.white;
+		Handles.DrawDottedLine (bezier.points.t0.position, bezier.points.t3.position, 5f);
+		DoPositionHandle (bezier.points.t0);
+		DoPositionHandle (bezier.points.t1);
+		DoPositionHandle (bezier.points.t2);
+		DoPositionHandle (bezier.points.t3);
+		Handles.DrawBezier (bezier.points.t0.position, bezier.points.t3.position, bezier.points.t1.position, bezier.points.t2.position, Color.white, null, 4f);
+		GUIStyle style = new GUIStyle ();
+		style.fontSize = 20;
+		Handles.Label (bezier.points.t0.position, "p0", style);
+		Handles.Label (bezier.points.t1.position, "p1", style);
+		Handles.Label (bezier.points.t2.position, "p2", style);
+		Handles.Label (bezier.points.t3.position, "p3", style);
+		for (int i = 0; i < STEPS; i++) {
+			float t = (float)i / STEPS;
+			Handles.color = Color.green;
+			Vector3 point = bezier.GetPoint (t);
+			Vector3 velocity = bezier.GetVelocity (t);
+			Handles.DrawLine (point, point + velocity.normalized * 2f);
+		}
+	}
+
+	void DoPositionHandle (Transform t)
+	{
+		EditorGUI.BeginChangeCheck ();
+		Vector3 pos = Handles.DoPositionHandle (t.position, Tools.pivotRotation == PivotRotation.Local ? t.rotation : Quaternion.identity);
+		if (EditorGUI.EndChangeCheck ()) {
+			Undo.RecordObject (t, "Move " + t.name);
+			EditorUtility.SetDirty (t);
+			t.position = pos;
+		}
+	}
+
+	public override void OnInspectorGUI ()
+	{
+		BezierController controller = (BezierController)target;
+
+		GUILayout.Button ("Hi");
+		DrawDefaultInspector ();
+
+		if (GUI.changed) {
+			Undo.SetCurrentGroupName ("Blah " + controller.name);
+			EditorSceneManager.MarkSceneDirty (EditorSceneManager.GetActiveScene ());
+		}
+
+		serializedObject.ApplyModifiedProperties ();
+	}
+
+}
