b895de73fda54c6385079137128c5a335866c67b
Fred Sauer <fredsa@google.com>
2016-01-16 17:42:49 -0800

    larger hint font size in magic window mode

---
+++ Assets/VrModeFontSizeController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(Text))]
+public class VrModeFontSizeController : MonoBehaviour
+{
+	public int vrModeFontSize = 6;
+	public int magicWindowFontSize = 12;
+
+	void Start ()
+	{
+		Prefs.OnVrModeChanged += UpdateFontSize;
+		UpdateFontSize (Prefs.VrMode);
+	}
+
+	void OnDestroy ()
+	{
+		Prefs.OnVrModeChanged -= UpdateFontSize;
+	}
+
+	void UpdateFontSize (bool vrMode)
+	{
+		gameObject.GetComponent<Text> ().fontSize = vrMode ? vrModeFontSize : magicWindowFontSize;
+	}
+
+}
