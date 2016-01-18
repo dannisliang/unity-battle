cd6fed4264e8150d60532be59f983a881187bcc2
Fred Sauer <fredsa@google.com>
2016-01-17 20:45:56 -0800

    locking to thread safe

--- Assets/_Scripts/1_Game/GameAi.cs
+++ Assets/_Scripts/1_Game/GameAi.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine.Assertions;
 
-public class GameAi :MonoBehaviour
+public class GameAi : MonoBehaviour
 {
-	List<Position> emptyPositions;
-	List<Position> unidentifiedHits;
+	object _lock = new System.Object ();
+
+	List<Position> _emptyPositions;
+	List<Position> _unidentifiedHits;
 
 	void Start ()
 	{
@@ -24,108 +26,129 @@ public class GameAi :MonoBehaviour
 
 	void Init ()
 	{
-		emptyPositions = new List<Position> ();
-		for (int i = 0; i < Utils.GRID_SIZE; i++) {
-			for (int j = 0; j < Utils.GRID_SIZE; j++) {
-				emptyPositions.Add (new Position (i, j));
+		lock (_lock) {
+			_emptyPositions = new List<Position> ();
+			for (int i = 0; i < Utils.GRID_SIZE; i++) {
+				for (int j = 0; j < Utils.GRID_SIZE; j++) {
+					_emptyPositions.Add (new Position (i, j));
+				}
 			}
+			_unidentifiedHits = new List<Position> ();
 		}
-		unidentifiedHits = new List<Position> ();
 	}
 
 	void NoteStrikeResult (Whose whose, Boat boat, Position position, StrikeResult result)
 	{
-		switch (result) {
-		case StrikeResult.MISS:
-		case StrikeResult.IGNORED_ALREADY_MISSED:
-		case StrikeResult.IGNORED_ALREADY_HIT:
-			return;
-		case StrikeResult.HIT_NOT_SUNK:
-			unidentifiedHits.Add (position);
-			return;
-		case StrikeResult.HIT_AND_SUNK:
-			for (int i = 0; i < boat.positions.Length; i++) {
-				Position pos = boat.GetPosition (i);
-				if (!pos.Equals (position)) {
-					Assert.IsTrue (unidentifiedHits.Remove (pos));
+		lock (_lock) {
+			switch (result) {
+			case StrikeResult.MISS:
+			case StrikeResult.IGNORED_ALREADY_MISSED:
+			case StrikeResult.IGNORED_ALREADY_HIT:
+				return;
+			case StrikeResult.HIT_NOT_SUNK:
+				_unidentifiedHits.Add (position);
+				return;
+			case StrikeResult.HIT_AND_SUNK:
+				for (int i = 0; i < boat.positions.Length; i++) {
+					Position pos = boat.GetPosition (i);
+					if (!pos.Equals (position)) {
+						Assert.IsTrue (_unidentifiedHits.Remove (pos));
+					}
 				}
+				return;
+			default:
+				throw new System.NotImplementedException ();
 			}
-			return;
-		default:
-			throw new System.NotImplementedException ();
 		}
 	}
 
 	public Position NextMove ()
 	{
-		Position pos = AimInLine () ?? AimAround () ?? AimRandom ();
-		emptyPositions.Remove (pos);
-		return pos;
+		lock (_lock) {
+			Position pos = AimInLine () ?? AimAround () ?? AimRandom ();
+			_emptyPositions.Remove (pos);
+			return pos;
+		}
 	}
 
 	Position AimInLine ()
 	{
-		foreach (Position hitPos in unidentifiedHits) {
-			Position pos = NextInLine (hitPos);
-			if (pos != null) {
-				return pos;
+		lock (_lock) {
+			foreach (Position hitPos in _unidentifiedHits) {
+				Position pos = NextInLine (hitPos);
+				if (pos != null) {
+					return pos;
+				}
 			}
+			return null;
 		}
-		return null;
 	}
 
 	Position NextInLine (Position hitPos)
 	{
-		return IfIsInLine (hitPos.Above (), hitPos.Above ().Above ())
-		?? IfIsInLine (hitPos.Below (), hitPos.Below ().Below ())
-		?? IfIsInLine (hitPos.Left (), hitPos.Left ().Left ())
-		?? IfIsInLine (hitPos.Right (), hitPos.Right ().Right ());
+		lock (_lock) {
+			return IfIsInLine (hitPos.Above (), hitPos.Above ().Above ())
+			?? IfIsInLine (hitPos.Below (), hitPos.Below ().Below ())
+			?? IfIsInLine (hitPos.Left (), hitPos.Left ().Left ())
+			?? IfIsInLine (hitPos.Right (), hitPos.Right ().Right ());
+		}
 	}
 
 	Position IfIsInLine (Position checkPosition, Position candidatePosition)
 	{
-		if (unidentifiedHits.Contains (checkPosition) && emptyPositions.Contains (candidatePosition)) {
-			return candidatePosition;
+		lock (_lock) {
+			if (_unidentifiedHits.Contains (checkPosition) && _emptyPositions.Contains (candidatePosition)) {
+				return candidatePosition;
+			}
+			return null;
 		}
-		return null;
 	}
 
 	Position AimAround ()
 	{
-		foreach (Position hitPos in unidentifiedHits) {
-			Position pos = NextEmptyPositionAround (hitPos);
-			if (pos != null) {
-				return pos;
+		lock (_lock) {
+			foreach (Position hitPos in _unidentifiedHits) {
+				Position pos = NextEmptyPositionAround (hitPos);
+				if (pos != null) {
+					return pos;
+				}
 			}
+			return null;
 		}
-		return null;
 	}
 
 	Position NextEmptyPositionAround (Position hitPos)
 	{
-		return IfIsOpenPosition (hitPos.Above ())
-		?? IfIsOpenPosition (hitPos.Below ())
-		?? IfIsOpenPosition (hitPos.Left ())
-		?? IfIsOpenPosition (hitPos.Right ());
+		lock (_lock) {
+			return IfIsOpenPosition (hitPos.Above ())
+			?? IfIsOpenPosition (hitPos.Below ())
+			?? IfIsOpenPosition (hitPos.Left ())
+			?? IfIsOpenPosition (hitPos.Right ());
+		}
 	}
 
 	Position IfIsOpenPosition (Position pos)
 	{
-		if (pos == null) {
+		lock (_lock) {
+			if (pos == null) {
+				return null;
+			}
+			foreach (Position emptyPos in _emptyPositions) {
+				if (pos.Equals (emptyPos)) {
+					return pos;
+				}
+			}
 			return null;
 		}
-		foreach (Position emptyPos in emptyPositions) {
-			if (pos.Equals (emptyPos)) {
-				return pos;
-			}
-		}
-		return null;
 	}
 
 	public Position AimRandom ()
 	{
-		var index = Random.Range (0, emptyPositions.Count - 1);
-		Position pos = emptyPositions [index];
-		return pos;
+		lock (_lock) {
+			var index = Random.Range (0, _emptyPositions.Count - 1);
+			Position pos = _emptyPositions [index];
+			return pos;
+		}
 	}
+
 }
