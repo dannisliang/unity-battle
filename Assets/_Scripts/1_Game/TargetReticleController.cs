69a4d8c6a308de8d7a488c262f4804157583f68f
Fred Sauer <fredsa@google.com>
2016-02-04 18:52:58 -0800

    aim reticle lerps to target

--- Assets/_Scripts/1_Game/TargetReticleController.cs
+++ Assets/_Scripts/1_Game/TargetReticleController.cs
@@ -3,12 +3,23 @@ using System.Collections;
 
 public class TargetReticleController : MonoBehaviour
 {
+	const float velocity = 8f;
+
+	Vector3 targetPos;
+
+	void Update ()
+	{
+		if (targetPos == null) {
+			return;
+		}
+		transform.localPosition = Vector3.Lerp (transform.localPosition, targetPos, Time.deltaTime * velocity);
+	}
 
 	public void SetTargetPosition (Position position, bool lockOnTarget)
 	{
 		gameObject.SetActive (position != null);
 		if (position != null) {
-			transform.localPosition = position.AsGridLocalPosition (Marker.Target);
+			targetPos = position.AsGridLocalPosition (Marker.Target);
 			if (lockOnTarget) {
 				GetComponent<AudioSource> ().Play ();
 			}
