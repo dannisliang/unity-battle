72524dc740749cfdd1e4c6f1cd97ccd104f43764
Fred Sauer <fredsa@google.com>
2016-02-07 10:58:03 -0800

    support 1/2 in for view mode selection

--- Assets/_Scripts/Rendering/ViewModeSelectionController.cs
+++ Assets/_Scripts/Rendering/ViewModeSelectionController.cs
@@ -6,8 +6,21 @@ using UnityEngine.EventSystems;
 public class ViewModeSelectionController : MonoBehaviour,IPointerDownHandler
 {
 	public bool vrMode;
+	public KeyCode keyCode;
 
 	public void OnPointerDown (PointerEventData eventData)
+	{
+		DoSelectVrMode ();
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (keyCode)) {
+			DoSelectVrMode ();
+		}
+	}
+
+	void DoSelectVrMode ()
 	{
 		Game.instance.SelectViewMode (vrMode);
 	}
