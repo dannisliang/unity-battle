9f413b978469c87f666c37650a7fb13195957597
Fred Sauer <fredsa@google.com>
2016-03-19 15:28:43 -0700

    show both fleets

--- Assets/_Scripts/1_Game/FleetPanelController.cs
+++ Assets/_Scripts/1_Game/FleetPanelController.cs
@@ -25,7 +25,11 @@ public class FleetPanelController : MonoBehaviour
 				GameObject clone = Game.instance.InstantiateTemp (shipPrefab);
 				Vector3[] fourCornersArray = new Vector3[4];
 				GetComponent<RectTransform> ().GetWorldCorners (fourCornersArray);
-				clone.transform.position = fourCornersArray [1] - (2.5f + i * 1.4f) * transform.up;
+				if (boatPlacementController.whose == Whose.Ours) {
+					clone.transform.position = fourCornersArray [2] - (2.5f + i * 1.4f) * transform.up - (float)boats [i].Size () * transform.right;
+				} else {
+					clone.transform.position = fourCornersArray [1] - (2.5f + i * 1.4f) * transform.up;
+				}
 				clone.transform.rotation = Quaternion.FromToRotation (-clone.transform.forward, -transform.forward);
 				clone.transform.localScale = new Vector3 ((float)boats [i].Size () / 5f, 1f, 1f);
 			}
