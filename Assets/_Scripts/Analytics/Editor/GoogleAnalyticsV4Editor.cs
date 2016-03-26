1b77fd30a90f261c7e84c752f3af1bdfa9e7dfdf
Fred Sauer <fredsa@google.com>
2016-03-26 15:32:20 -0700

    mark scene dirty when updating analytics

--- Assets/_Scripts/Analytics/Editor/GoogleAnalyticsV4Editor.cs
+++ Assets/_Scripts/Analytics/Editor/GoogleAnalyticsV4Editor.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System.Collections;
+using UnityEditor.SceneManagement;
 
 [CustomEditor (typeof(GoogleAnalyticsV4))]
 public class GoogleAnalyticsV4Editor : Editor
@@ -17,13 +18,27 @@ public class GoogleAnalyticsV4Editor : Editor
 	void CheckValues ()
 	{
 		GoogleAnalyticsV4 gav4 = (GoogleAnalyticsV4)target;
-		gav4.androidTrackingCode = TRACKING_CODE;
-		gav4.IOSTrackingCode = TRACKING_CODE;
-		gav4.otherTrackingCode = TRACKING_CODE;
-		gav4.productName = PlayerSettings.productName;
-		gav4.bundleIdentifier = PlayerSettings.bundleIdentifier;
-		gav4.bundleVersion = PlayerSettings.bundleVersion;
-		serializedObject.ApplyModifiedProperties ();
+		bool dirty = false;
+		dirty |= VerifyValue (ref gav4.androidTrackingCode, TRACKING_CODE);
+		dirty |= VerifyValue (ref gav4.IOSTrackingCode, TRACKING_CODE);
+		dirty |= VerifyValue (ref gav4.otherTrackingCode, TRACKING_CODE);
+		dirty |= VerifyValue (ref gav4.productName, PlayerSettings.productName);
+		dirty |= VerifyValue (ref gav4.bundleIdentifier, PlayerSettings.bundleIdentifier);
+		dirty |= VerifyValue (ref gav4.bundleVersion, PlayerSettings.bundleVersion);
+		if (dirty) {
+			Debug.LogWarning ("***" + target.name + " updated; Marking scene dirty");
+			serializedObject.ApplyModifiedProperties ();
+			EditorSceneManager.MarkSceneDirty (EditorSceneManager.GetActiveScene ());
+		}
+	}
+
+	bool VerifyValue (ref string field, string value)
+	{
+		if (!field.Equals (value)) {
+			field = value;
+			return true;
+		}
+		return false;
 	}
 
 }
