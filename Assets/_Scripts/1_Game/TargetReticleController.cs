a81e07124507fc10c892af44fb714660f6fbcad5
Fred Sauer <fredsa@google.com>
2016-03-27 11:13:59 -0700

    alternate magic window reticle material

--- Assets/_Scripts/1_Game/TargetReticleController.cs
+++ Assets/_Scripts/1_Game/TargetReticleController.cs
@@ -4,22 +4,42 @@ using System.Collections;
 [RequireComponent (typeof(Animator), typeof(CardboardAudioSource))]
 public class TargetReticleController : MonoBehaviour
 {
+	public Material materialVrMmode;
+	public Material materialMagicWindow;
+
 	const float velocity = 8f;
 
+	MeshRenderer meshRenderer;
 	Animator animator;
 	CardboardAudioSource audioSource;
 	Vector3 targetPos;
 
 	void Awake ()
 	{
+		meshRenderer = GetComponentInChildren<MeshRenderer> ();
 		animator = GetComponent<Animator> ();
 		audioSource = GetComponent<CardboardAudioSource> ();
 		gameObject.SetActive (false);
 	}
 
+	void OnEnable ()
+	{
+		Game.instance.OnGameStateChange += HandleGameStateChange;
+	}
+
+	void OnDisable ()
+	{
+		Game.instance.OnGameStateChange -= HandleGameStateChange;
+	}
+
 	void Update ()
 	{
 		transform.localPosition = Vector3.Lerp (transform.localPosition, targetPos, Time.deltaTime * velocity);
+	}
+
+	void HandleGameStateChange (GameState state)
+	{
+		meshRenderer.material = Cardboard.SDK.VRModeEnabled ? materialVrMmode : materialMagicWindow;
 	}
 
 	public void SetTargetPosition (Position position, bool lockOnTarget)
